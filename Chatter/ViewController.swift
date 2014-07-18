f5a7be1668dbae10b8a04d18f5131db0be3c7479
qmchenry <quinn@jqm.us>
2014-07-17 21:21:36 -0400

    Initial Commit

---
+++ Chatter/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Chatter
+//
+//  Created by Quinn McHenry on 7/17/14.
+//  Copyright (c) 2014 Small Planet Digital. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+                                    
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+                                    
+    }
+
+
+}
+
---
+++ Chatter/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Chatter
+//
+//  Created by Quinn McHenry on 7/17/14.
+//  Copyright (c) 2014 Small Planet Digital. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
